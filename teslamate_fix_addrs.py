b88bb92d714944d8af1d01cc7e8274384cdb1480
h4rK8s <63350517+h4rk8s@users.noreply.github.com>
2024-09-08 14:39:32 +0800

    Update teslamate_fix_addrs.py
    add more header to fixed OSM's Access blocked

--- teslamate_fix_addrs.py
+++ teslamate_fix_addrs.py
@@ -257,6 +257,19 @@ def http_request(url):
     http_session.mount('http://', HTTPAdapter(max_retries=args.retry))
     http_session.mount('https://', HTTPAdapter(max_retries=args.retry))
     headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'cache-control': 'max-age=0',
+        'dnt': '1',
+        'priority': 'u=0, i',
+        'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
         'User-Agent': args.user_agent
     }
 
